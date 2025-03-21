2110fd5bde6e977992d651d0943dd3ac08c2586f
Deepali <deepalinavgire7350@gmail.com>
2025-03-21 12:33:16 +0530

    Revert "keeping single msg"
    This reverts commit 298658639489135ba6f66cbfe0d5c4e860b06e85.

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello,I did one more modification!");
+Console.WriteLine("Hello,I did one more modification!");
+Console.WriteLine("Hello,I did one more modification!");
+Console.WriteLine("Hello,I did one more modification!");
+Console.WriteLine("Hello,I did one more modification!");
+Console.WriteLine("Hello,I did one more modification!");
+Console.WriteLine("Hello,I did one more modification!");
+Console.WriteLine("Hello,I did one more modification!");
+Console.WriteLine("Hello,I did one more modification!");
