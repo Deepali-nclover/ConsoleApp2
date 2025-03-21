19798932ebf9899feeea270a4f79ff8a8ddb3ff0
Deepali <deepalinavgire7350@gmail.com>
2025-03-21 13:43:10 +0530

    Initial commit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.VisualBasic;
+
 Console.WriteLine("Hello,I did one more modification!");
 Console.WriteLine("Hello,I did one more modification!");
 Console.WriteLine("Hello,I did one more modification!");
@@ -8,3 +10,20 @@ Console.WriteLine("Hello,I did one more modification!");
 Console.WriteLine("Hello,I did one more modification!");
 Console.WriteLine("Hello,I did one more modification!");
 Console.WriteLine("Hello,I did one more modification!");
+
+void myfunc1()
+{
+    Console.WriteLine("First function");
+}
+void myfunc2()
+{
+    Console.WriteLine("First function");
+}
+void myfunc3()
+{
+    Console.WriteLine("First function");
+}
+void myfunc4()
+{
+    Console.WriteLine("First function");
+}
