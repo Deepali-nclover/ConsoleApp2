298658639489135ba6f66cbfe0d5c4e860b06e85
Deepali-nclover <141030840+Deepali-nclover@users.noreply.github.com>
2025-03-21 12:27:58 +0530

    keeping single msg
    updated Program.cs file

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,10 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello,I did one more modification!");
-Console.WriteLine("Hello,I did one more modification!");
-Console.WriteLine("Hello,I did one more modification!");
-Console.WriteLine("Hello,I did one more modification!");
-Console.WriteLine("Hello,I did one more modification!");
-Console.WriteLine("Hello,I did one more modification!");
-Console.WriteLine("Hello,I did one more modification!");
-Console.WriteLine("Hello,I did one more modification!");
-Console.WriteLine("Hello,I did one more modification!");
